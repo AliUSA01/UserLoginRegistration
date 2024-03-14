db8bb089f414d872743f3842fc555d5e15a01eae
AliUSA01 <alimammadov873@gmail.com>
2024-03-14 10:05:04 +0400

    user set. Login and registration db ready

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,21 @@
+package entity;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Table(name = "users")
+@Entity
+@FieldDefaults(level=AccessLevel.PRIVATE)
+public class User {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    Integer id;
+     String userName;
+     String password;
+    String repeatPassword;
+     String email;
+}
